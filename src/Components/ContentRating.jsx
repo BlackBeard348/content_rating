b055b3d496669f5a901475fb817f64e43fc7a5a3
BlackBeard348 <140602646+BlackBeard348@users.noreply.github.com>
2025-02-12 07:10:45 +0800

    Update ContentRating.jsx
    Added a parent component

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,10 @@
 
-import React, { Component } from 'react';
-import './ContentRating.css';
-
-class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
-}
-
-export default ContentRating;
+import ContentRating from "./Components/ContentRating"
+ function App() {
+   return (
+    <>
+   <ContentRating/>
+    </>
+   )
+ }
+ export default App
